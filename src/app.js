fb551a6ccf7a0fbeb0eebb24ec4533df9b8a8ce2
jayknight7535 <thewitchjay@gmail.com>
2025-05-09 18:35:21 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,30 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+window.onload = function() {}
+  //write your code  
+  const suits=['♥️', '♦️', '♠️', '♣️']
+   function randomizedsuit(suits){
+     let suit = Math.floor(Math.random() * suits.length);
+    return suits[suit]}
+  let Suits =randomizedsuit(suits)
+   document.querySelector('#topSuit').innerHTML= Suits
+    document.querySelector('#revSuit').innerHTML= Suits  
+    
+  const ranks=['2','3','4','5','6','7','8','9','J','Q','K','A']
+   function randomizedrank(ranks){
+    let rank = Math.floor(Math.random() * ranks.length);
+    return ranks[rank]}
+    document.querySelector('#Rank').innerHTML = randomizedrank(ranks)
+ 
+  
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+ 
+  
+
+
+  
+
+ 
+  
+  
